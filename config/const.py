b7b128821fe786d37ea6d6f7624218ab2c51ab58
Maroš Motaj <79695352+MarosMotaj@users.noreply.github.com>
2022-08-31 20:20:45 +0200

    Add files via upload

---
+++ config/const.py
@@ -0,0 +1,8 @@
+IP_ADDRESS = 'broker.emqx.io'
+PORT = 1883
+TOPIC_VOLTS = "esp32/mptt-volts"
+TOPIC_AMPER = "esp32/mptt-amper"
+TOPIC_POWER = "esp32/mptt-power"
+CLIENT_ID = ""
+USERNAME = ''
+PASSWORD = ''
